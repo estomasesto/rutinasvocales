1538134e752efb8417f44f4f75a9977026fa0acf
estomasesto <tomasigau@gmail.com>
2025-07-11 15:01:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,28 +3,32 @@ const trackSelect = document.getElementById('trackSelect');
 const speedSlider = document.getElementById('speedSlider');
 const speedLabel = document.getElementById('speedLabel');
 
-const tracks = [
-  { name: "Ejercicio 1 - Vocales", file: "audios/ejercicio1.mp3" },
-  { name: "Ejercicio 2 - Sirenas", file: "audios/ejercicio2.mp3" }
-];
+// Cargar la lista desde playlist.json
+fetch('playlist.json')
+  .then(response => response.json())
+  .then(tracks => {
+    tracks.forEach(track => {
+      const option = document.createElement('option');
+      option.textContent = track.name;
+      option.value = track.file;
+      trackSelect.appendChild(option);
+    });
 
-tracks.forEach(track => {
-  const option = document.createElement('option');
-  option.textContent = track.name;
-  option.value = track.file;
-  trackSelect.appendChild(option);
-});
+    if (tracks.length > 0) {
+      trackSelect.value = tracks[0].file;
+      audioPlayer.src = tracks[0].file;
+    }
+  });
 
+// Cambiar de pista
 trackSelect.addEventListener('change', () => {
   audioPlayer.src = trackSelect.value;
   audioPlayer.play();
 });
 
+// Cambiar velocidad
 speedSlider.addEventListener('input', () => {
   const speed = parseFloat(speedSlider.value);
   speedLabel.textContent = speed.toFixed(1) + "x";
   audioPlayer.playbackRate = speed;
 });
-
-trackSelect.value = tracks[0].file;
-audioPlayer.src = trackSelect.value;
